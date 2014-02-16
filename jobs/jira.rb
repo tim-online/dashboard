6bb669e1ad26148400b8a0ff871090922a11b44f
Leon Bogaert <leon@tim-online.nl>
2014-02-16 16:35:09 +0100

    Filter jira issues a different way

--- jobs/jira.rb
+++ jobs/jira.rb
@@ -14,7 +14,7 @@ client = JIRA::Client.new(options)
 # issues = client.Issue.jql('duedate > startOfWeek() AND duedate < endOfWeek() ORDER BY due ASC')
 
 SCHEDULER.every '5m', :first_in => 0 do |job|
-  issues = client.Issue.jql('duedate > startOfWeek() AND duedate < endOfWeek() ORDER BY due ASC')
+  issues = client.Issue.jql('duedate >= now() ORDER BY due ASC')
   issues = issues[0..15]
   issues = issues.map do |issue|
     { label: issue.duedate, value: issue.summary }
