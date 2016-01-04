7d213f87c6b7985a9a18f9448c737d45d72e4d20
Leon Bogaert <leon@tim-online.nl>
2016-01-04 13:37:14 +0100

    added mysql backup date

--- jobs/borgbackup.rb
+++ jobs/borgbackup.rb
@@ -6,26 +6,30 @@ SCHEDULER.every '12h', :first_in => 0 do |job|
   #   {
   #     "repo": "ironhide.tim-online.nl",
   #     "date": "2012-04-23T18:25:43.511Z"
+  #     "mysql_date": "2012-04-23T18:25:43.511Z"
   #   },
   #   {
   #     "repo": "starscream.tim-online.nl",
   #     "date": "2013-04-23T18:25:43.511Z"
+  #     "mysql_date": "2013-04-23T18:25:43.511Z"
   #   },
   #   {
   #     "repo": "mirage.tim-online.nl",
   #     "date": "2013-04-23T18:25:43.511Z"
+  #     "mysql_date": ""
   #   }
   # ]'
 
   url = 'http://backups.tim-online.nl:2674/recent'
-  buffer = open(url).read
+  buffer = open(url, :read_timeout => 60*10).read
   items = JSON.parse(buffer)
 
   # rows = items.take(6).map do |item|
   rows = items.map do |item|
     item['date'] = Time.parse(item['date'])
     diff_days = (Date.today - item['date'].to_datetime).to_i
-    {
+
+    ret = {
       'cols' => [
         {
           'value' => item['repo'],
@@ -37,15 +41,31 @@ SCHEDULER.every '12h', :first_in => 0 do |job|
           'title' => '',
           'class' => diff_days > 2 ? 'icon-warning-sign' : '',
         },
+        {
+          'value' => '-',
+          'title' => '',
+          'class' => '',
+        },
       ]
     }
+
+    if item['mysql_date'] and item['mysql_date'] != ''
+      item['mysql_date'] = Time.parse(item['mysql_date'])
+      mysql_diff_days = (Date.today - item['mysql_date'].to_datetime).to_i
+
+      ret['cols'][2]['value'] = item['mysql_date'].strftime('%d-%m-%Y')
+      ret['cols'][2]['class'] = mysql_diff_days > 2 ? 'icon-warning-sign' : ''
+    end
+
+    ret
   end
 
   send_event('borgbackup', {
     rows: rows,
     headers: [
       'Repo',
-      'Datum'
+      'Datum',
+      'Mysql Datum'
     ]
   })
 end
